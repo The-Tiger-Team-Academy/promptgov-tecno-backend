6debf5ad11abef957dd9c8dc523c8c9d0c769b8c
BestTTTA <158189243+BestTTTA@users.noreply.github.com>
2024-02-08 15:55:40 +0000

    update

--- module/database.py
+++ module/database.py
@@ -3,7 +3,7 @@ import os
 
 # เก็บเอาไว้กันลืม
 client =  MongoClient(os.getenv("MONGO_URL"))
-# client =  MongoClient("mongodb+srv://promtgov:promtgov@cluster0.b1xky3c.mongodb.net/")#รอเชื่อมกับฐานข้อมูล จริงๆต้องเชื่อมกับฐานข้อมูลที่อยู่บนเซิฟเวอร์
 
-db = client['mongo-db']
+db = client['example']
 users_collection = db["users"]
+
