fedbe1592120822bbf5cdc1f097bdc532928253e
BestTTTA <158189243+BestTTTA@users.noreply.github.com>
2024-02-08 16:34:55 +0000

    update

--- services/create_doc_messagerecord.py
+++ services/create_doc_messagerecord.py
@@ -7,7 +7,7 @@ import os
 
 # Initialize Google Cloud Storage Client
 storage_client = storage.Client.from_service_account_json("storage-admin.json")
-bucket_name = "storagepromtgovfile"
+bucket_name = "tecno-storage"
 
 router = APIRouter(
     tags=["Create Services"],
